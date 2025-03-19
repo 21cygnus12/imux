53c47be940ad946b7e1e34b33e02a1216cdbb18e
21cygnus12 <zack.waterman@watermancs.com>
2025-03-19 12:31:33 -0700

    working, no changes yet

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /mnt/c/Users/zackg/projects/imux/src
+cp -r ~/projects/imux/src /mnt/c/Users/zackg/projects/imux/src
+
+rm /mnt/c/Users/zackg/projects/imux/Cargo.toml
+cp ~/projects/imux/Cargo.toml /mnt/c/Users/zackg/projects/imux/Cargo.toml 
